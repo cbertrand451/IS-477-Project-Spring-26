37045f5ee9b81c8894b783cf2d48b5f13ca25565
cbertrand451 <colinboebelbertrand@gmail.com>
2026-05-06 23:01:55 -0500

    final run all

--- run_all.py
+++ run_all.py
@@ -28,10 +28,12 @@ def main():
 
     # acquire data
     run("scripts/acquire/acquire_draft_data.py")
+    # WARNING!! This script may take 2-5 minutes to run as it scrapes basketball-reference.com for rookie stats
+    # I set a manual timer in between each scrape so that the IP address of the user doesn;t get banned
+    # basketball-reference.com has a 1 hour ban for super fast botting, I believe
     run("scripts/acquire/acquire_rookie_data.py")
     # manual season dataset only needs checksum creation
     run("scripts/acquire/chicago_bulls_season_data_sha256.py")
-
     # check the integrity of the data using checksum and sha
     run("scripts/acquire/check_sha256.py")
 
@@ -39,8 +41,15 @@ def main():
     # clean and combine draft picks with rookie-year stats
     run("scripts/clean/clean_draft_data.py")
     run("scripts/clean/clean_bulls_data.py")
+    # integrate the now cleaned datasets
     run("scripts/integrate/integrate_cleaned_datasets.py")
+    # clean the integrated dataset for analysis
     run("scripts/clean/clean_integrated_data.py")
+
+    # run the eda analyzing
+    run("scripts/analyze/dataset_eda.py")
+    # create the model and visualizations
+    run("scripts/analyze/modeling.py")
 
     
     print("\nWorkflow finished\n")
